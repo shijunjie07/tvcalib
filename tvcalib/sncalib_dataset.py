5f170b3c0409fb4442c800bb1cd81b22f7d5a13b
shijunjie07 <shijunja@yahoo.com>
2024-11-29 14:22:25 +0800

    Update sncalib_dataset.py

--- tvcalib/sncalib_dataset.py
+++ tvcalib/sncalib_dataset.py
@@ -400,7 +400,7 @@ def custom_list_collate(batch):
             # If we're in a background process, concatenate directly into a
             # shared memory tensor to avoid an extra copy
             numel = sum(x.numel() for x in batch)
-            storage = elem.storage()._new_shared(numel)
+            storage = elem.untyped_storage()._new_shared(numel)
             out = elem.new(storage).resize_(len(batch), *list(elem.size()))
         return torch.stack(batch, 0, out=out)
     elif (
